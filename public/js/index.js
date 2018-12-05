34eb44305614748569952e94f5742417545660ce
Yokesh <byokeshb@gmail.com>
2018-12-05 12:20:44 +0530

    create msg listener event

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket=io();
+
+socket.on('connect',function(){
+  console.log('Connected to server');
+
+  
+});
+
+socket.on('disconnect',function(){
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage',function(msg){
+  console.log('New msg',msg);
+});
