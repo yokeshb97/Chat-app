94a2a71ea2157928c25559c1289bd01473a83ea3
Yokesh <byokeshb@gmail.com>
2018-12-05 11:54:27 +0530

    Add new msg and create msg events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from:'yoks',
+    text:'asasdasd',
+    createdAt:123
+
+  });
+
+  socket.on('createMessage',(msg)=>{
+    console.log('msg',msg);
+  });
+
   socket.on('disconnect',()=>{
     console.log('User disconnected');
   });
