2f332132ba1b2f00be844a71c8b199671ed4c11a
Yokesh <byokeshb@gmail.com>
2018-12-05 12:19:19 +0530

    emit create msg

--- server/server.js
+++ server/server.js
@@ -14,15 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log('New user connected');
 
-  socket.emit('newMessage',{
-    from:'yoks',
-    text:'asasdasd',
-    createdAt:123
-
-  });
-
-  socket.on('createMessage',(msg)=>{
+socket.on('createMessage',(msg)=>{
     console.log('msg',msg);
+    io.emit('newMessage',{
+      from:msg.from,
+      text:msg.text,
+      createdAt:new Date().getTime()
+    })
   });
 
   socket.on('disconnect',()=>{
