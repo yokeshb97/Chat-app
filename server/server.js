d614993aa40edd2c47e9e0bfa54a374e181948fb
Yokesh <byokeshb@gmail.com>
2018-12-05 12:45:01 +0530

    Create new users and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
   console.log('New user connected');
+  socket.emit('newMessage',{
+    from:'admin',
+    text:'Welcome to the chat app',
+    createdAt:new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from:'admin',
+    text:'New user joined',
+    createdAt:new Date().getTime()
+  });
 
 socket.on('createMessage',(msg)=>{
     console.log('msg',msg);
@@ -20,7 +30,13 @@ socket.on('createMessage',(msg)=>{
       from:msg.from,
       text:msg.text,
       createdAt:new Date().getTime()
-    })
+    });
+    /*socket.broadcast.emit('newMessage',{
+      from:msg.from,
+      text:msg.text,
+      createdAt:new Date().getTime()
+    });*/
+
   });
 
   socket.on('disconnect',()=>{
