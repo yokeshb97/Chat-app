29c11aaf96458a90bfdbe59974652d66c18c3ea9
Yokesh <byokeshb@gmail.com>
2018-12-05 22:19:34 +0530

    Modified UI

--- public/js/index.js
+++ public/js/index.js
@@ -35,7 +35,7 @@ jQuery('#message-form').on('submit',function(e){
     from:'User',
     text:jQuery('[name=message]').val()
   },function(){
-
+    jQuery('[name=message]').val('')
   });
 });
 
@@ -44,12 +44,18 @@ locationButton.on('click',function(){
 if(!navigator.geolocation){
     return alert('Geolocation not suported by your browser');
 }
+locationButton.attr('disabled','disabled').text('Sending location...');
+
+
 navigator.geolocation.getCurrentPosition(function(position){
+  locationButton.removeAttr('disabled').text('Send location');
+
   socket.emit('createLocationMessage',{
     latitude:position.coords.latitude,
     longitude:position.coords.longitude
 });
 },function(){
+  locationButton.removeAttr('disabled').text('Send location');
   alert('Unable to fetch location');
 });
 });
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection',(socket)=>{
 socket.on('createMessage',(msg,callback)=>{
     console.log('msg',msg);
     io.emit('newMessage',generateMessage(msg.from,msg.text));
-    callback('This is from the server');
+    callback();
   });
   socket.on('createLocationMessage',(coords)=>{
       io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
